8cff4faea48fa57579f81df2ae7f50bdcdcc76e7
Ruben Epong <rubenjohne@gmail.com>
2013-12-03 14:25:43 -0800

    first commit with capistrano

---
+++ config/deploy.rb
@@ -0,0 +1,38 @@
+require "bundler/capistrano"
+require 'fast_git_deploy/enable'
+
+set :application, "mochi"
+set :repository,  "https://github.com:rubenjohne/mochi.git"
+set :deploy_to, "/u/apps/#{application}"
+set :scm, :git
+
+set :user, "vagrant"
+set :branch, "master"
+set :deploy_type, 'deploy'
+set :use_sudo, false
+
+default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
+ssh_options[:keys] = [File.join(ENV["HOME"], ".vagrant.d", "insecure_private_key")]
+
+role :app, "mochi.test"
+role :web, "mochi.test"
+role :db,  "mochi.test", :primary => true
+
+after "deploy:setup" do
+  deploy.fast_git_setup.clone_repository
+  run "cd #{current_path} && bundle install"
+end
+
+namespace :unicorn do
+  desc "Start unicorn for this application"
+  task :start do
+    run "cd #{current_path} && bundle exec unicorn -c /etc/unicorn/mochi.conf.rb -D"
+  end
+end
+
+namespace :deploy do
+  task :symlink do
+  # no-op to remove default symlink task, not needed by fast_git_deploy
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mochi::Application.config.secret_key_base = '0e79b8954e88ecf9f8e93bea5a9f85ddaf1e4234898a4dff8303df1961ed3957ff72819630f95d9bfaa9b0898175655d7df6534d988c686dcce61579a90336ef'
